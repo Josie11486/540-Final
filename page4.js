9f29013ff7056e6c9daa086d93ffb8d514528be1
tiffanydemps <35701572+tiffanydemps@users.noreply.github.com>
2018-12-03 14:34:31 -0500

    update
    try location

--- page4.js
+++ page4.js
@@ -38,6 +38,19 @@ function draw() {
   image(capture, 0, 0, 320, 240);
   filter('INVERT');
   
+   ellipse(707, 700, 30, 30);
+    ellipse(707, 750, 30, 30);
+
+  textAlign(CENTER, CENTER);
+  textSize(40);
+  text("What type of student are you?", 800, 800);
+  textAlign(LEFT, LEFT);
+  textSize(25);
+  text("3. Question", x, y-100);
+  textSize(20);
+  text("A.  Answer", x, y);
+  text("B.  Answer", x, y+50);
+  
   stroke(255);
   y += speed;
   ellipse(x,y,x,y+20);
@@ -50,19 +63,7 @@ function draw() {
     y = 0;
   }
   
-  
- ellipse(707, 700, 30, 30);
-    ellipse(707, 750, 30, 30);
-
-  textAlign(CENTER, CENTER);
-  textSize(40);
-  text("What type of student are you?", 800, 800);
-  textAlign(LEFT, LEFT);
-  textSize(25);
-  text("3. Question", x, y-100);
-  textSize(20);
-  text("A.  Answer", x, y);
-  text("B.  Answer", x, y+50);
+ 
 
   
 if(mouseIsPressed) {
