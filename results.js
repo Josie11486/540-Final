c3693b1ff983053d79c545dc1a54e0da3db7b806
Josie <jaa287@miami.edu>
2018-12-03 13:16:48 -0500

    numbers

--- results.js
+++ results.js
@@ -15,8 +15,26 @@ function draw() {
   rect(0, 0, width, height);
   pop();
 
-  var choice = getCookie();
-  text(choice, 240, 120);
+  var total = 0;
+  var choice1 = getCookie('index_page1=');
+  var choice2 = getCookie('index_page2=');
+  var choice3 = getCookie('index_page3=');
+  var choice4 = getCookie('index_page4=');
+  var choice5 = getCookie('index_page5=');
+  var choice6 = getCookie('index_page6=');
+  print(choice1);
+  print(choice2);
+  print(choice3);
+  print(choice4);
+  print(choice5);
+  print(choice6);
+  total += parseInt(choice1);
+  total += parseInt(choice2);
+  total += parseInt(choice3);
+  total += parseInt(choice4);
+  total += parseInt(choice5);
+  total += parseInt(choice6);
+  text(total, 240, 120);
 
   // Title, instructions, answers, activity space
 textSize(40);
@@ -26,13 +44,13 @@ text("Result Page", 700, 180);
 }
 
 //https://www.w3schools.com/js/js_cookies.asp
-function getCookie() {
-  var name = "index_page1=";
+function getCookie(name) {
+  /*var name = "index_page1=";
   var name = "index_page2=";
   var name = "index_page3=";
   var name = "index_page4=";
   var name = "index_page5=";
-  var name = "index_page6=";
+  var name = "index_page6=";*/
   var decodedCookie = decodeURIComponent(document.cookie);
   var ca = decodedCookie.split(";");
   for(var i = 0; i <ca.length; i++) {
