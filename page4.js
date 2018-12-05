42f8bd4b7600204cd270ed380aab581c8f686590
Josie11486 <31413862+Josie11486@users.noreply.github.com>
2018-12-05 18:59:11 -0500

    paper background

--- page4.js
+++ page4.js
@@ -1,60 +1,67 @@
 var button;
 var choice = '0';
-var x = 300;
-var y = 300;
 var arrow;
-var y = 0;
-var speed = -5;
+var paper;
+var paper2;
+
 
 function preload() {
   arrow = loadImage('arrow.png');
+  paper = loadImage('paper.jpg')
+  paper2 = loadImage('paper2.jpg')
 }
 
 function setup() {
-  createCanvas(1400, 700);
+  createCanvas(1500, 1400);
   background(255);
   textAlign(CENTER, CENTER);
   button = createButton('Next');
-  button.position(650, 600);
+  button.position(650, 620);
   button.mousePressed(nextPage);
   button.size(100,50);
   button.style('font-size', '20px');
-  
+  button.style("background-color","#ffffff");
+  button.style("border", "2px solid black");
 }
 
 
 function draw() {
-  push()
-  noStroke();
-  rect(0, 0, width, height);
-  pop();
- 
-  
-  stroke(255);
-  y += speed;
-  ellipse(x,y,x,y+20);
-  ellipse(2*x, y-30, 2*x, y+20-30);
-  ellipse(3*x, y+50, 3*x, y+20+50);
-  ellipse(4*x, y-10, 4*x, y+20-10);
-  x= random(0, width);
+push()
+noStroke();
+rect(0, 0, width, height);
+pop();
 
-  if(y > height) {
-    y = 0;
-  }
-  
- 
+image(paper, 0, 0);
+image(paper2, 1000, 0);
 
-  
+push()
+noFill();
+ellipse(363, 372, 40, 40);
+ellipse(363, 442, 40, 40);
+pop();
+
+textAlign(CENTER, CENTER);
+textSize(40);
+text("What type of student are you?", 700, 100);
+textAlign(LEFT, LEFT);
+textSize(30);
+text("4. Question?", 300, 298);
+
+//Delete between these comments when you add your activity
+text("A. For your activities, when you create your if-and statement, make sure that", 350, 371);
+text("B. the planner situation has a choice = '1' and procrastinator says choice = '2'", 350, 444);
+
 if(mouseIsPressed) {
-  if(mouseX>=292 && mouseX<=322 && mouseY>=285 && mouseY<=315) {
+  if(mouseX>=343 && mouseX<=383 && mouseY>=352 && mouseY<=392) {
     choice = '1';
-    image(arrow, 230, 275);
+    image(arrow, 285, 347);
 
-  } else if(mouseX>=292 && mouseX<=322 && mouseY>=335 && mouseY<=365) {
+  } else if(mouseX>=343 && mouseX<=383 && mouseY>=422 && mouseY<=462) {
       choice = '2';
-      image(arrow, 230, 325);
+      image(arrow, 285, 420);
   }
 }
+//Delete between these comments when you add your activity
 }
 
 function nextPage() {
@@ -67,5 +74,5 @@ function saveCookie() {
   var d = new Date();
   d.setTime(d.getTime() + 60*60*1000);
   var expires = "expires=" + d.toUTCString();
-  document.cookie = "index_page4=" + choice + ";" + expires + ";path=/";
+  document.cookie = "index_page1=" + choice + ";" + expires + ";path=/";
 }
