fdaa93177ac958208fc0efa7e0b909a68961d231
Josie11486 <31413862+Josie11486@users.noreply.github.com>
2018-12-05 18:57:00 -0500

    paper background

--- page1.js
+++ page1.js
@@ -1,7 +1,5 @@
 var button;
 var choice = '0';
-var x = 300;
-var y = 300;
 var arrow;
 var paper;
 var paper2;
@@ -18,10 +16,12 @@ function setup() {
   background(255);
   textAlign(CENTER, CENTER);
   button = createButton('Next');
-  button.position(650, 600);
+  button.position(650, 620);
   button.mousePressed(nextPage);
   button.size(100,50);
   button.style('font-size', '20px');
+  button.style("background-color","#ffffff");
+  button.style("border", "2px solid black");
 }
 
 
@@ -31,30 +31,32 @@ noStroke();
 rect(0, 0, width, height);
 pop();
 
- ellipse(307, 300, 30, 30);
- ellipse(307, 350, 30, 30);
-  
- image(paper, 0, 0);
- image(paper2, 1000, 0);
+image(paper, 0, 0);
+image(paper2, 1000, 0);
+
+push()
+noFill();
+ellipse(363, 372, 40, 40);
+ellipse(363, 442, 40, 40);
+pop();
 
 textAlign(CENTER, CENTER);
 textSize(40);
-text("What type of student are you?", 700, 40);
+text("What type of student are you?", 700, 100);
 textAlign(LEFT, LEFT);
-textSize(25);
-text("1. You have a free weekend, how do you choose to spend it?", x, y-100);
-textSize(20);
-text("A.  Get ahead of the homework for next week so I won't be stressed all week", x, y);
-text("B.  Take advantage of all the free time. Tailgate, parties and sleeping in!", x, y+50);
+textSize(30);
+text("1. You have a free weekend, how do you choose to spend it?", 300, 298);
+text("A.  Get ahead of the homework for next week so I won't be stressed all week.", 350, 371);
+text("B.  Take advantage of all the free time. Tailgate, parties and sleeping in!", 350, 444);
 
 if(mouseIsPressed) {
-  if(mouseX>=292 && mouseX<=322 && mouseY>=285 && mouseY<=315) {
+  if(mouseX>=343 && mouseX<=383 && mouseY>=352 && mouseY<=392) {
     choice = '1';
-    image(arrow, 230, 275);
+    image(arrow, 285, 347);
 
-  } else if(mouseX>=292 && mouseX<=322 && mouseY>=335 && mouseY<=365) {
+  } else if(mouseX>=343 && mouseX<=383 && mouseY>=422 && mouseY<=462) {
       choice = '2';
-      image(arrow, 230, 325);
+      image(arrow, 285, 420);
   }
 }
 }
