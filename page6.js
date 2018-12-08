07ec0b5270b5f59580b88cdadb8cc4854ecd1bc0
Josie11486 <31413862+Josie11486@users.noreply.github.com>
2018-12-07 19:45:39 -0500

    Update page6.js

--- page6.js
+++ page6.js
@@ -33,34 +33,13 @@ pop();
 
 image(paper, 0, 0);
 image(paper2, 1000, 0);
+  
+  
 
-push()
-noFill();
-ellipse(363, 372, 40, 40);
-ellipse(363, 442, 40, 40);
-pop();
+//PUT YOUR ACTIVITY HERE//
 
-textAlign(CENTER, CENTER);
-textSize(40);
-text("What type of student are you?", 700, 100);
-textAlign(LEFT, LEFT);
-textSize(30);
-text("6. Question?", 300, 298);
-//Delete between these comments when you add your activity
-text("A. For your activities, when you create your if-and statement, make sure that", 350, 371);
-text("B. the planner situation has a choice = '1' and procrastinator says choice = '2'", 350, 444);
-
-if(mouseIsPressed) {
-  if(mouseX>=343 && mouseX<=383 && mouseY>=352 && mouseY<=392) {
-    choice = '1';
-    image(arrow, 285, 347);
-
-  } else if(mouseX>=343 && mouseX<=383 && mouseY>=422 && mouseY<=462) {
-      choice = '2';
-      image(arrow, 285, 420);
-  }
-}
-//Delete between these comments when you add your activity
+  
+  
 }
 
 function nextPage() {
